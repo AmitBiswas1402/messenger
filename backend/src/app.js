4991eaf37dac05029ebe03def56d8c2d09491660
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-02-15 20:54:20 +0530

    Implement authentication and messaging routes; add user profile update and message handling functionality

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
 
 import { connectDB } from './db/db.js';
 import authRoutes from './routes/auth.routes.js'; 
+import messageRoutes from './routes/message.routes.js'; 
 
 dotenv.config();
 const app = express();
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const PORT = 5000;
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);   
