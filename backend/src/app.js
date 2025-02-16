5280b407b72005783ee4c6cd75da00ad200b950c
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-02-16 21:11:43 +0530

    Update CORS configuration to allow requests from the frontend and remove redundant settings

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,19 +11,17 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const PORT = 5000;
 
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true, 
+}));
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
