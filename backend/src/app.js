915d179e8a133e41dce38fd2c369657059d1b93c
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-02-16 16:55:10 +0530

    Add initial frontend pages and components; integrate Tailwind CSS and Axios for API calls

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,24 +1,31 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import { connectDB } from './db/db.js';
-import authRoutes from './routes/auth.routes.js'; 
-import messageRoutes from './routes/message.routes.js'; 
+import { connectDB } from "./db/db.js";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 
 dotenv.config();
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const PORT = 5000;
 
-app.use("/api/auth", authRoutes)
-app.use("/api/message", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);   
-    connectDB(); 
-})
+  console.log(`Server running on http://localhost:${PORT}`);
+  connectDB();
+});
