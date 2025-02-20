be1c0318f5bab3eb10d177597992957405b86d11
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-02-20 20:36:21 +0530

    Add social-3 package and update project structure for improved deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./db/db.js";
 import authRoutes from "./routes/auth.routes.js";
@@ -14,15 +15,24 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 app.use(cookieParser());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:3000",
   credentials: true, 
 }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
